c57bb831870b78da61b324082986282089bff898
Brad Greenlee <brad@footle.org>
2012-03-04 18:37:31 -0800

    Prepend our libs to the system path so that they take precedence over installed libs.
    Fixes issue #9.

--- sublime_requests.py
+++ sublime_requests.py
@@ -1,6 +1,6 @@
 import sys
 import os.path
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib"))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib"))
 import re
 import requests
 from requests.status_codes import codes
